4e375f7716ffdc9e7779f8faea5be9b02d11371a
smartdev <igorlahoveckij49@example.com>
2024-02-22 05:59:44 +1000

    second commit

---
+++ public/data/navbar-data.ts
@@ -0,0 +1,122 @@
+const NavbarData = [
+  {
+    title: "Home",
+    submenu: [
+      // {
+      //   title: "Ai Solution",
+      //   path: "/",
+      // },
+      // {
+      //   title: "Ai Chatbot",
+      //   path: "/index-two",
+      // },
+      // {
+      //   title: "Ai Gaming",
+      //   path: "/index-three",
+      // },
+      // {
+      //   title: "Ai Prompts",
+      //   path: "/index-four",
+      // },
+      {
+        title: "My Portfolio",
+        path: "/index-five",
+      },
+      // {
+      //   title: "Ai News",
+      //   path: "/index-six",
+      // },
+      {
+        title: "Our Agency",
+        path: "/index-seven",
+      },
+    ],
+  },
+  {
+    title: "Works",
+    path: "/about-us",
+  },
+  // {
+  //   title: "Project",
+  //   submenu: [
+  //     {
+  //       title: "Shop",
+  //       path: "/shop",
+  //     },
+  //     {
+  //       title: "Product Details",
+  //       path: "/product-single",
+  //     },
+  //     {
+  //       title: "Cart",
+  //       path: "/cart",
+  //     },
+  //     {
+  //       title: "Checkout",
+  //       path: "/checkout",
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: "Pages",
+  //   submenu: [
+  //     {
+  //       title: "Error",
+  //       path: "/error",
+  //     },
+  //     {
+  //       title: "Sign In",
+  //       path: "/sign-in",
+  //     },
+  //     {
+  //       title: "Services",
+  //       subInSub: [
+  //         {
+  //           title: "Our Services",
+  //           path: "/services",
+  //         },
+  //         {
+  //           title: "Service Details",
+  //           path: "/service-single",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       title: "Case Study",
+  //       subInSub: [
+  //         {
+  //           title: "Case Study",
+  //           path: "/case-study",
+  //         },
+  //         {
+  //           title: "Case Study Details",
+  //           path: "/case-study-single",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       title: "Create Account",
+  //       path: "/sign-up",
+  //     },
+  //   ],
+  // },
+  {
+    title: "resume",
+    submenu: [
+      {
+        title: "Blog",
+        path: "/blog",
+      },
+      // {
+      //   title: "Blog Details",
+      //   path: "/blog-single",
+      // },
+    ],
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+];
+
+export default NavbarData;
